0954d44a960ea4164d3345e2c4c56ec2dc34fcc0
indusy <teririkawaii@outlook.com>
2022-05-31 20:08:26 +0800

    practice

---
+++ 18-easy-tuple-length/test-cases.ts
@@ -0,0 +1,16 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+
+// const 声明常量，不能改变，所以 as const 会将类型变成元组
+const tesla = ['tesla', 'model 3', 'model X', 'model Y'] as const
+const spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT'] as const
+
+type hh = Length<typeof tesla>
+
+type cases = [
+  Expect<Equal<Length<typeof tesla>, 4>>,
+  Expect<Equal<Length<typeof spaceX>, 5>>,
+  // @ts-expect-error
+  Length<5>,
+  // @ts-expect-error
+  Length<'hello world'>,
+]
